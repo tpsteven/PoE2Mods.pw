7d5b158f571a93d311c097e9eeebb4dcd84d0c8e
Tyler Stevens <12705595+tpstevens@users.noreply.github.com>
2018-05-18 22:54:56 -0700

    Possible fix to allow retreating on first action. Still should adjust morale penalty on first action.

---
+++ Mods/ShipMechanicsMod/mod_Conditionals.cs
@@ -0,0 +1,23 @@
+﻿using Patchwork;
+
+using Game;
+using Game.GameData;
+using Onyx;
+
+namespace ShipMechanicsMod
+{
+    [ModifiesType]
+    class mod_Conditionals
+    {
+        [ModifiesMember("IsAtShipRetreatDistance")]
+        public static bool mod_IsAtShipRetreatDistance()
+        {
+            ShipDuelState state = SingletonBehavior<ShipDuelManager>.Instance.CurrentState;
+
+            return state.IsFirstTurnOfRound 
+                   || state.Distance >= ShipDuelSettingsGameData.Instance.RetreatDistance;
+
+            // return SingletonBehavior<ShipDuelManager>.Instance.CurrentState.Distance >= ShipDuelSettingsGameData.Instance.RetreatDistance;
+        }
+    }
+}
